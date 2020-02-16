84043a357c29c4fdb1013e689d702e6698b95f01
Barney666 <540747702@qq.com>
2020-02-17 00:32:13 +0800

    在？为什么和商院同学夸下海口说加积分商城问题不大？

--- miniprogram/pages/shop/shop.js
+++ miniprogram/pages/shop/shop.js
@@ -1,66 +1,32 @@
-// pages/shop/shop.js
+
+var data=require("../../shopData.js")
+
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    dataList:[],   
+    // leftMenuList:[],
+    rightContent:[],
+    currentIndex:0    //被点击的左侧菜单
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({ dataList: data.JsonData.message} );
+    this.setData({ rightContent: data.JsonData.message[0].children });
+    // this.setShopData();
+    // let leftMenuList = this.dataList.map(v => v.cat_name);
+    // this.setData({ leftMenuList });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  //直接在wxml里做处理了
+  // setShopData(){
+  //   let leftMenuList = this.dataList.map(v => v.cat_name);
+  //   this.setData({ leftMenuList });
+  // }
 
-  },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
