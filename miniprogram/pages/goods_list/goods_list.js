efeb520f397312e7fe4e8dd515ffbdaab67098bd
Barney666 <540747702@qq.com>
2020-02-20 02:21:50 +0800

    添加每一类商品点击后的页面

---
+++ miniprogram/pages/goods_list/goods_list.js
@@ -0,0 +1,43 @@
+// pages/goods_list/goods_list.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    tabs:[
+      {
+        id:0,
+        value:"综合",
+        isActive:true
+      },
+      {
+        id: 1,
+        value: "销量",
+        isActive: false
+      },
+      {
+        id: 2,
+        value: "价格",
+        isActive: false
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(options)
+  },
+
+  //标题点击事件 从子组件传递过来
+  handleTabsItemChange(e){
+    const {index} = e.detail;
+    let {tabs}=this.data;
+    tabs.forEach((v,i)=>i===index?v.isActive=true:v.isActive=false);
+    this.setData({tabs})
+  }
+
+ 
+})
--- miniprogram/pages/shop/shop.js
+++ miniprogram/pages/shop/shop.js
@@ -1,32 +1,56 @@
 
-var data=require("../../shopData.js")
-
-
 Page({
 
   data: {
     dataList:[],   
     // leftMenuList:[],
     rightContent:[],
-    currentIndex:0    //被点击的左侧菜单
+    currentIndex:0,    //被点击的左侧菜单
+    scrollTop:0    //右侧滚动条距离顶部的距离
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({ dataList: data.JsonData.message} );
-    this.setData({ rightContent: data.JsonData.message[0].children });
-    // this.setShopData();
-    // let leftMenuList = this.dataList.map(v => v.cat_name);
-    // this.setData({ leftMenuList });
+    const json=wx.getStorageSync("json");
+    if(!json){
+      this.getJson();
+    }
+    else{  //有旧的数据  定义过期时间 5min
+      if( Date.now()-json.time > 5*1000*60  ){  //过期了
+        this.getJson(); 
+      }
+      else{    //旧数据没过期
+        this.json=json.data;
+        this.setData({ dataList: this.json.JsonData.message });
+        this.setData({ rightContent: this.json.JsonData.message[0].children });
+      }
+    } 
   },
 
-  //直接在wxml里做处理了
-  // setShopData(){
-  //   let leftMenuList = this.dataList.map(v => v.cat_name);
-  //   this.setData({ leftMenuList });
-  // }
+  //获取json数据
+  getJson(){
+    this.json = require("../../shopData.js");
+    wx.setStorageSync("json", { time: Date.now(), data: this.json });
+    this.setData({ dataList: this.json.JsonData.message });
+    this.setData({ rightContent: this.json.JsonData.message[0].children });
+  },
+
+  //左侧菜单的点击事件
+  handleItemTap(e){
+    const {index} = e.currentTarget.dataset;//获取被点击的标题身上的索引
+    let rightContent = this.json.JsonData.message[index].children;
+    this.setData({
+      currentIndex: index,      //给data中的currentIndex赋值
+      rightContent,
+      scrollTop: 0    //右侧滚动条距离顶部的值
+    })
+    
+    
+  }
+
+
 
 
 })
