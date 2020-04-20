e3b7d1f03e552d75d77b0caa24b3c31a5e5cc7e0
Barney666 <540747702@qq.com>
2020-04-20 17:01:56 +0800

    正式首页完成，亲子账户页面完成

---
+++ miniprogram/pages/login/login.js
@@ -0,0 +1,9 @@
+Page({
+  handleGetUserInfo(e) {
+    const { userInfo } = e.detail;
+    wx.setStorageSync("userInfo", userInfo);
+    wx.navigateBack({
+      delta: 1
+    });
+  }
+})
--- miniprogram/pages/parent_child/parent_child.js
+++ miniprogram/pages/parent_child/parent_child.js
@@ -7,12 +7,9 @@ Page({
     userInfo:{}
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
+
   onShow: function () {
-    const userInfo=wx.getStorageSync("userInfo");
+    const userInfo=wx.getStorageSync("userInfo");    // 这块得先判断是否已经登陆
     this.setData({userInfo})
   },
-
 })
